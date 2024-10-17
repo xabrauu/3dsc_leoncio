92b9c36d38a47bb29685222d3b77484cd87325f8
xabrauu <130366440+xabrauu@users.noreply.github.com>
2024-10-17 08:47:25 -0300

    operadorterminario

---
+++ operadorternario/index.js
@@ -0,0 +1,8 @@
+const idade = 18;
+if (idade >= 18) {
+  console.log("permitida a entrada");
+} else {
+  console.log("proibido para menores");
+}
+  const passe = idade >= 18 ? "permitida a entrada" : "proibida a entrada"
+console.log(passe)
