13477288d852f8eeec4ddb15d4c2190345cd1d42
xabrauu <130366440+xabrauu@users.noreply.github.com>
2024-10-17 08:53:26 -0300

    LoopWhile

---
+++ LoopWhile/index.js
@@ -0,0 +1,22 @@
+let n = 1
+
+while(n <= 10) {//LOOP INFINITO -- CTRL+C PARA PARAR
+  console.log(n);
+  n++;
+-------------------------------------------
+let n = 1
+
+while(true){
+  let n2 = (Math.random() * 60).toFixed(2);
+  console.log(n2)
+  console.clear();
+
+}
+----------------------------------------------
+do{
+  console.log("-------MENU-------");
+  console.log("QUAL É O SEU NOME?");
+  console.log("------------------");
+  n = n + 1 ;
+} while(n<10)
+}
