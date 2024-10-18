7920f48ae01240400f29d2170327392e5a2098a7
xabrauu <130366440+xabrauu@users.noreply.github.com>
2024-10-18 19:17:22 -0300

    LastingHomelyNaturaldocs

---
+++ LastingHomelyNaturaldocs/index.js
@@ -0,0 +1,12 @@
+const login = "Brnu"
+const senha = "35791"
+let l, s = ""
+while (l != login || s != senha) {
+  l = prompt("digite o login: ")
+  s = prompt("digite a senha: ")
+  if (l != login || s != senha){
+    alert("LOgin ou senha incorretos")
+    break
+  }
+  window.location.replace("www.alura.com.br")
+}
